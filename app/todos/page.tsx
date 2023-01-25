489c7c3d511ae0795778b7e9593c0f4d8dc719d4
sc-ryanv2 <ryan@snowcrash.com>
2023-01-25 00:07:36 -0800

    layout within layout

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,10 +1,9 @@
-import TodosList from "./TodosList";
-
 function Todos() {
   return (
     <div>
-      {/*@ts-ignore*/}
-      <TodosList />
+      <div>
+        <h1>This is where the Todoswill be listed...</h1>
+      </div>
     </div>
   );
 }
